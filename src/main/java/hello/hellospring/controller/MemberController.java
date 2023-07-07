5e540349aa107b55214fedc8cdfb5caaea49f27d
ljs7143 <junseo27643779@gmail.com>
2023-07-08 00:35:24 +0900

    h2DB(ing. . .)

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller //스프링이 관리함 -> 스프링 빈에 등록 -> 스프링 컨테이너에 등록해서 스프링이 관리할 수 있도록 함 .
 public class MemberController {
@@ -21,8 +24,8 @@ public class MemberController {
     public String createForm(){
         return  "members/createMemberForm";
     }
-    @PostMapping
-    public  String create(MemberForm form){
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
         Member member = new Member();
         member.setName(form.getName());
 
@@ -30,6 +33,17 @@ public class MemberController {
 
         return  "redirect:/";    //회원가입이 끝났으니 홈화면으로 보내는 것임
     }
+    @GetMapping("/members")
+    public String list(Model model){
+         List<Member> members  = memberService.findMembers();
+         model.addAttribute("members", members);
+         return  "members/memberList";
+         //list() 메소드에서는 MemberService의 findMembers() 메소드를 호출하여 회원 목록을 가져온 다음, model.addAttribute("members", members)를
+        // 사용하여 members라는 이름으로 Model 객체에 회원 목록을 추가합니다. 이렇게 하면, 뷰에서 members라는 이름으로 Model 객체에 추가된 회원 목록에 접근할 수 있게 됩니다.
+
+    }
+
+
 
 
 
